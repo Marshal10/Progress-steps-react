466a86461abecd1145a8b9b5405ededc675f1342
Marshal10 <marshalmaleane123@gmail.com>
2024-05-21 20:38:40 +0530

    Html markup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 export default function App() {
   return (
-    <div className="container">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <button className="close-btn">&times;</button>
+      <div className="container">
+        <div className="steps">
+          <div className="step">1</div>
+          <div className="step">2</div>
+          <div className="step">3</div>
+        </div>
+        <div className="message">
+          <p>Step 1:Learn React</p>
+        </div>
+        <div className="actionBtns">
+          <button className="btn">Previous</button>
+          <button className="btn">Next</button>
+        </div>
+      </div>
+    </>
   );
 }
