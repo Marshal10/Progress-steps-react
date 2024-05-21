51b00f69d9fb38b9b8f6284d1442805b8279255b
Marshal10 <marshalmaleane123@gmail.com>
2024-05-21 22:31:27 +0530

    Open and close container

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handleNext() {
     if (step < 3) setStep((s) => s + 1);
@@ -16,29 +17,38 @@ export default function App() {
   function handlePrevious() {
     if (step > 1) setStep((s) => s - 1);
   }
+
+  function toggleContainer() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <>
-      <div className="container">
-        <button className="close-btn">&times;</button>
-        <div className="steps">
-          <div className={step >= 1 ? "active" : ""}>1</div>
-          <div className={step >= 2 ? "active" : ""}>2</div>
-          <div className={step >= 3 ? "active" : ""}>3</div>
+      <button className="close-btn" onClick={toggleContainer}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="container">
+          <div className="steps">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <div className="message">
+            <p>
+              Step {step}: {messages[step - 1]}
+            </p>
+          </div>
+          <div className="actionBtns">
+            <button className="btn" onClick={handlePrevious}>
+              Previous
+            </button>
+            <button className="btn" onClick={handleNext}>
+              Next
+            </button>
+          </div>
         </div>
-        <div className="message">
-          <p>
-            Step {step}: {messages[step - 1]}
-          </p>
-        </div>
-        <div className="actionBtns">
-          <button className="btn" onClick={handlePrevious}>
-            Previous
-          </button>
-          <button className="btn" onClick={handleNext}>
-            Next
-          </button>
-        </div>
-      </div>
+      )}
     </>
   );
 }
