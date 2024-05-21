035eee1f88e764577ffc76100d3e73ed8a87b34d
Marshal10 <marshalmaleane123@gmail.com>
2024-05-21 22:07:16 +0530

    Active classes toggling

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ const messages = [
 ];
 
 export default function App() {
+  const step = 2;
   return (
     <>
       <div className="container">
         <button className="close-btn">&times;</button>
         <div className="steps">
-          <div className="step active">1</div>
-          <div className="step">2</div>
-          <div className="step">3</div>
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
         </div>
         <div className="message">
           <p>Step 1:Learn React</p>
